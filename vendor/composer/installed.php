faadad63985eaa742d1fe7335745b5205d9ab251
danielplebani <danielpleba03@gmail.com>
2023-10-26 15:09:16 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pacificdev/laravel_9_preset' => array(
+            'pretty_version' => '1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => '984848f4e7f3855eade53759a91bc3f769a0e308',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pacificdev/laravel_9_preset',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
